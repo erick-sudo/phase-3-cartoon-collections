d970649fac58dbfd932cbeb9c8cbbfb16ed1fc59
Erick Ochieng <erickochieng766@gmail.com>
2023-02-13 07:39:54 +0300

    Passed Test

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs) # code an argument here
   # Your code here
+  dwarfs.each.with_index { |dwarf, index| puts "#{index+1}. #{dwarf.capitalize}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.map.with_index { |call, index| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.grep(/\b.{5,}\b/).length > 0
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find { |snack| cheese_types.include? snack}
 end
